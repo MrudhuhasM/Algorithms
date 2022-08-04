7a04f3c9cd493f1c26b941834e05f81ec82d50f7
MrudhuhasM <mrudhuhas@outlook.com>
2022-08-04 10:49:39 +0530

    Added Double Linked List

--- lists/lists.py
+++ lists/lists.py
@@ -1,18 +1,29 @@
+from typing import Any, Union
+
+from requests import delete
+
+
 class Node:
     """
     Class representing a node
     """
 
     def __init__(self) -> None:
+        """
+        Constructor for Node
+        """
         self._data = None
         self._next = None
         self._prev = None
 
-    def check_type(method):
-        def wrapper_(self,list_type):
-            if list_type == LinkedList():
-                raise TypeError('Function not allowed in  linked list')
-        return wrapper_
+    # def check_type(method):
+    #     """
+    #     Checks what class a node belongs to so we can diable prev for linked list
+    #     """
+    #     def wrapper_(self):
+    #         if self.list_type == LinkedList():
+    #             raise TypeError('Function not allowed in  linked list')
+    #     return wrapper_
 
     @property
     def list_type(self):
@@ -42,17 +53,18 @@ class Node:
         return self._next != None
 
     @property
-    @check_type
     def prev(self):
         return self._prev
     
     @prev.setter
-    @check_type
     def prev(self,prev):
         self._prev = prev
 
    
 class LinkedList:
+    """
+    Class to represent Linked List
+    """
 
     def __init__(self) -> None:
         self._length =0
@@ -73,10 +85,10 @@ class LinkedList:
     def length(self):
         raise TypeError("Not Allowed")
 
-    def insertAtBegining(self,data):
+    def insertAtBegining(self,data)-> None:
         new_node = Node()
         new_node.data = data
-        new_node.list_type = LinkedList
+        # new_node.list_type = LinkedList
         if self._length > 0:
             currentnode = self._head
             self._head = new_node
@@ -85,10 +97,9 @@ class LinkedList:
             self._head = new_node
         self._length += 1
 
-    def insertAtEnd(self,data):
+    def insertAtEnd(self,data: Any)-> None:
         new_node = Node()
         new_node.data = data
-        new_node.list_type = LinkedList
         if self._length > 0:
             currentnode = self._head
             while currentnode.next != None:
@@ -98,10 +109,9 @@ class LinkedList:
         else:
             self.insertAtBegining(data)
     
-    def insertAtMiddle(self,pos,data):
+    def insertAtMiddle(self,pos: int,data: Any)-> None:
         new_node = Node()
         new_node.data = data
-        new_node.list_type = LinkedList
         if pos > self._length or pos< 0:
             raise IndexError('Position out of index')
         elif self._length == 0:
@@ -120,7 +130,7 @@ class LinkedList:
             new_node.next = currentnode
             self._length += 1
 
-    def check_list_empty(method):
+    def check_list_empty(method)-> None:
         def wrapper_(self):
             if self._length >= 0:
                 method(self)
@@ -129,14 +139,14 @@ class LinkedList:
         return wrapper_
     
     @check_list_empty
-    def deleteAtBegining(self):
+    def deleteAtBegining(self)-> None:
         currenthead =self._head
         self._head = currenthead.next
         self._length -=1
 
     
     @check_list_empty
-    def deleteAtEnd(self):
+    def deleteAtEnd(self)-> None:
         previoushead = self._head
         currenthead = self._head
         while currenthead.next != None:
@@ -146,7 +156,7 @@ class LinkedList:
         self._length -= 1        
 
     
-    def deleteAtNode(self,pos):
+    def deleteAtNode(self,pos: int)-> None:
         if self._length >= 0:
             if pos > self._length or pos < 0:
                 raise IndexError("index out of range")
@@ -162,6 +172,129 @@ class LinkedList:
                 self._length -= 1
         else:
             raise ValueError("Empty List")
+
+
+class DoubleLinkedList:
+    def __init__(self) -> None:
+        self._length =0
+        self._head = None
+        self._tail = None
+
+    @property
+    def head(self):
+        return self._head.data
+
+    @head.setter
+    def head(self):
+        raise TypeError("Not allowed")
+
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self):
+        raise TypeError("Not Allowed")
+
+    def check_list_empty(method)-> None:
+        def wrapper_(self):
+            if self._length >= 0:
+                method(self)
+            else:
+                raise ValueError("Empty List")
+        return wrapper_
+    
+    def insertAtBegining(self,data: Any) -> None:
+        new_node = Node()
+        new_node.data = data
+        if self._head == None:
+            self._head = self._tail = new_node
+        else:
+            new_node.prev = None
+            new_node.next = self._head
+            self._head.prev = new_node
+            self._head = new_node
+        self._length += 1
+    
+    def insertAtEnd(self,data: Any)-> None:
+        new_node = Node()
+        new_node.data = data
+        if self._head == None:
+            self._head = self._tail = new_node
+        else:
+            current = self._head
+            while current.next != None:
+                current = current.next
+            new_node.prev = current
+            new_node.next = None
+        self._length += 1
+    
+    def insertAtMiddle(self,pos: int,data: Any) -> None:
+        new_node = Node()
+        new_node.data = data
+        if pos > self._length or pos < 0:
+            raise IndexError("Invalid Position")
+        elif self._length == 0:
+            self.insertAtbegining(data)
+        elif pos == self._length:
+            self.insertAtEnd(data)
+        else:
+            count = 0
+            current = self._head
+            while count < pos:
+                current = current.next
+                count += 1
+            previous = current.prev
+            previous.next = new_node
+            new_node.prev = previous
+            new_node.next = current
+            current.prev = new_node
+            self._length += 1
+    
+    @check_list_empty
+    def deleteAtBegining(self)-> None:
+        current = self._head
+        self._head = current.next
+        self._head.prev = None
+        self._length -= 1
+
+    @check_list_empty
+    def deleteAtEnd(self) -> None:
+        if self._length == 1:
+            self.deleteAtBegining()
+        else:
+            current = self._head
+            while current.next != None:
+                current = current.next
+            previous = current.prev
+            previous.next = None
+            self._length -= 1
+    
+    def deleteAtNode(self,pos: int) -> None:
+        if self._length == 0:
+            raise ValueError("Empty list")
+        elif self._length == 1:
+            self.deleteAtBegining()
+        elif self._length == pos:
+            self.deleteAtEnd()
+        else:
+            count = 0
+            current = self._head
+            while count < pos:
+                current = current.next
+                count += 1
+            previous = current.prev
+            current_next = current.next
+            previous.next = current_next
+            current_next.prev = previous
+            self._length -= 1
+
+
+
+
+
+
+
 
 
 
---
+++ test/test_list.py
@@ -0,0 +1,59 @@
+import unittest
+from lists.lists import LinkedList,DoubleLinkedList
+
+
+
+def initialize_linkedlist(type):
+    li1 = type
+    li1.insertAtBegining(1)
+    li1.insertAtBegining(2)
+    li1.insertAtBegining(3)
+    li1.insertAtBegining(4)
+    li1.insertAtBegining(5)
+    li1.insertAtBegining(6)
+    li1.insertAtBegining(7)
+    li1.insertAtBegining(8)
+    li1.insertAtEnd(0)
+
+    return li1
+
+
+class TestLinkedList(unittest.TestCase):
+    def test_insertion(self):
+        new_list = initialize_linkedlist(LinkedList())
+        new_list.insertAtBegining(9)
+        new_list.insertAtEnd(0)
+        new_list.insertAtMiddle(4,6)
+
+        self.assertEqual(new_list.head , 9)
+        self.assertEqual(new_list.length,12)
+
+
+    def test_deletion(self):
+        new_list = initialize_linkedlist(LinkedList())
+        new_list.deleteAtBegining()
+        new_list.deleteAtEnd()
+        new_list.deleteAtNode(3)
+
+        self.assertEqual(new_list.head , 7)
+        self.assertEqual(new_list.length,6)
+
+class TestDoubleList(unittest.TestCase):
+    def test_insertion(self):
+        new_list = initialize_linkedlist(DoubleLinkedList())
+        new_list.insertAtBegining(9)
+        new_list.insertAtEnd(0)
+        new_list.insertAtMiddle(4,6)
+
+        self.assertEqual(new_list.head , 9)
+        self.assertEqual(new_list.length,12)
+
+
+    def test_deletion(self):
+        new_list = initialize_linkedlist(DoubleLinkedList())
+        new_list.deleteAtBegining()
+        new_list.deleteAtEnd()
+        new_list.deleteAtNode(3)
+
+        self.assertEqual(new_list.head , 7)
+        self.assertEqual(new_list.length,6)
